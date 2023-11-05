f433263378e3c66c6684ed14d54cefdcf45a1de4
TYPSwift <aallani@ufl.edu>
2023-11-05 13:52:33 -0500

    meeting day

---
+++ Lesson 13/main.js
@@ -0,0 +1,4 @@
+const toProperCase = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    };
+    console.log(toProperCase("april"));
---
+++ Lesson 9/main.js
@@ -0,0 +1,13 @@
+let playerOne = "rock";
+let computer = "rock";
+let result =
+playerOne === computer
+? "Tie game!"
+: playerOne === "rock" && computer === "paper"
+? "Computer wins!"
+: playerOne === "paper" && computer === "scissors"
+? "Computer wins!"
+: playerOne === "scissors" && computer === "rock"
+? "Computer wins!"
+:"playerOne wins!";
+console.log(result);
